35343aa42eb12347cdcdc617d60f2bc340d4ca67
ChenCMD <scnmed.g@gmail.com>
2021-06-23 21:14:32 +0900

    :adhesive_bandage: パラメータに不正な値が入るのを修正

--- src/components/ParameterBox.tsx
+++ src/components/ParameterBox.tsx
@@ -12,7 +12,7 @@ const ParameterBox: React.FC<ParameterBoxProps> = ({ name, description, updatePa
     const [argValue, setArgValue] = useState(value);
     const onChange = (newValue: string) => {
         setArgValue(newValue);
-        if (newValue === '' || Number.isNaN(newValue)) return;
+        if (!(/^[+,-]?(?:[1-9]\d*|0)(?:\.\d+)?$/.test(newValue))) return;
         updateParam(newValue);
     };
 
