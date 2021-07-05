0743d40771bf6051b044537517c34696ac5d8444
ChenCMD <scnmed.g@gmail.com>
2021-07-05 15:32:02 +0900

    :children_crossing: 選択している図形がpreviewで前面に表示されるように

--- src/components/Previewer.tsx
+++ src/components/Previewer.tsx
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 import { Stage, Layer, Rect, Circle, Line } from 'react-konva';
 import Measure from 'react-measure';
 import styles from '../styles/Previewer.module.scss';
+import { GridMode } from '../types/GridMode';
 import { IdentifiedPoint } from '../types/Point';
 
 interface PreviewerProps {
     shapePoints: { selected: boolean, points: IdentifiedPoint[] }[]
+    gridMode: GridMode
 }
 
-const Previewer: React.FC<PreviewerProps> = ({ shapePoints }) => {
+const Previewer: React.FC<PreviewerProps> = ({ shapePoints, gridMode }) => {
     const [size, setSize] = useState<number>(100);
     const centerModifier = size / 2;
 
@@ -23,7 +25,12 @@ const Previewer: React.FC<PreviewerProps> = ({ shapePoints }) => {
 
     const points: JSX.Element[] = [], grids: JSX.Element[] = [];
     if (posMultiple && maxBounds < 250) {
-        points.push(...shapePoints.map(shape =>
+        points.push(...shapePoints.sort((a, b) => {
+            if (a.selected === b.selected) return 0;
+            if (a.selected) return 1;
+            if (b.selected) return -1;
+            return 0;
+        }).flatMap(shape =>
             shape.points.map(({ pos: [x, y], id }) => (
                 <Circle
                     x={x * posMultiple + centerModifier}
@@ -35,27 +42,30 @@ const Previewer: React.FC<PreviewerProps> = ({ shapePoints }) => {
                     key={id}
                 />
             ))
-        ).flat());
+        ));
 
-        const drawGridLine = (axis: 'x' | 'y', offset: number) =>
-            grids.push(
-                <Line
-                    key={`${axis}-${offset}`}
-                    points={axis === 'x'
-                        ? [0, offset + centerModifier, size, offset + centerModifier]
-                        : [offset + centerModifier, 0, offset + centerModifier, size]
-                    }
-                    stroke={offset === 0 ? 'rgb(96,96,96)' : 'rgb(48, 48, 48)'}
-                    strokeWidth={1.5}
-                />
-            );
-        for (let i = 0; i * posMultiple < size / 2; i++) {
-            const linePos = i * posMultiple;
-            drawGridLine('x', linePos);
-            drawGridLine('y', linePos);
-            if (i === 0) continue;
-            drawGridLine('x', -linePos);
-            drawGridLine('y', -linePos);
+        if (gridMode !== GridMode.off) {
+            const drawGridLine = (axis: 'x' | 'y', offset: number) =>
+                grids.push(
+                    <Line
+                        key={`${axis}-${offset}`}
+                        points={
+                            axis === 'x'
+                                ? [0, offset + centerModifier, size, offset + centerModifier]
+                                : [offset + centerModifier, 0, offset + centerModifier, size]
+                        }
+                        stroke={offset === 0 ? 'rgb(96,96,96)' : 'rgb(48, 48, 48)'}
+                        strokeWidth={1.5}
+                    />
+                );
+            for (let i = gridMode === GridMode.center ? 0 : 0.5; i * posMultiple < size / 2; i++) {
+                const linePos = i * posMultiple;
+                drawGridLine('x', linePos);
+                drawGridLine('y', linePos);
+                if (linePos === -linePos) continue;
+                drawGridLine('x', -linePos);
+                drawGridLine('y', -linePos);
+            }
         }
     }
 
