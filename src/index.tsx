c7ae3a2ecfed7c293ff3f3e0f479f8f850fe6260
ChenCMD <scnmed.g@gmail.com>
2021-07-13 03:14:48 +0900

    :egg: yvテーマを追加

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import ShapesGenerator from './components/ShapesGenerator';
 import './styles/global.scss';
 
+const params = window.location.search.substring(1).split('&').reduce<Record<string, string>>((obj, param) => {
+  const [key, val] = param.split('=');
+  obj[key] = val;
+  return obj;
+}, {});
+
+if (params.theme)
+  document.documentElement.setAttribute('theme', params.theme);
+
 ReactDOM.render(
   <React.StrictMode>
     <ShapesGenerator />
