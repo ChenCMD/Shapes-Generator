c85fe8aa1c7c2b9fa7014ab2358455abf46d85de
ChenCMD <scnmed.g@gmail.com>
2021-07-14 13:54:39 +0900

    :sparkles: ベジェ補正値に対応

--- src/ShapeNodes/Line.ts
+++ src/ShapeNodes/Line.ts
@@ -31,9 +31,20 @@ export class LineShape extends AbstractShapeNode<LineParams> {
     protected updatePointSet(params: Record<LineParams, number>): void {
         const points: IdentifiedPoint[] = [];
         const addPoint = (pos: Point) => points.push(createIdentifiedPoint(this.uuid, pos));
+        const calcPoint = (fromPos: Point, toPos: Point, t: number): Point => [
+            (1 - t) * fromPos[0] + t * toPos[0],
+            (1 - t) * fromPos[1] + t * toPos[1]
+        ];
 
-        for (let t = 0; t < 1; t += 1 / params.count)
-            addPoint([(1 - t) * params.from_x + t * params.to_x, (1 - t) * params.from_y + t * params.to_y]);
+        const vector = [params.to_x - params.from_x, params.to_y - params.from_y];
+        const vecMagnitude = Math.sqrt(vector[0] ** 2 + vector[1] ** 2);
+        const normalizedVector = [vector[1] / vecMagnitude * params.vezier, -vector[0] / vecMagnitude * params.vezier];
+
+        const controlPoint: Point = [(params.from_x + params.to_x) / 2 + normalizedVector[0], (params.from_y + params.to_y) / 2 + normalizedVector[1]];
+        for (let i = 0; i < params.count; i++) {
+            const t = i / (params.count - 1);
+            addPoint(calcPoint(calcPoint([params.from_x, params.from_y], controlPoint, t), calcPoint(controlPoint, [params.to_x, params.to_y], t), t));
+        }
 
         this.pointSet = points;
     }
