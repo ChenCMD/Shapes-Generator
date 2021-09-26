be7ddc39c1d8c932493ce4d45e90501da61bc2b1
ChenCMD <46134240+ChenCMD@users.noreply.github.com>
2021-09-26 23:19:48 +0900

    :bug: 最新バージョンのImportKeyを読み込めないのを修正 (#34)
    Fix #33

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,10 +1,9 @@
 import { ExportObject } from '../types/ExportObject';
 import { migrations } from './migrations';
 
-
 export function migrate(obj: { [k: string]: unknown }[], ver: number, targetVer: number): ExportObject[] {
-    const newObj = migrations.find(v => v.version === ver)?.migrator(obj) ?? [];
-    return ver + 1 === targetVer
+    const newObj = migrations.find(v => v.version === ver)?.migrator(obj) ?? obj;
+    return ver + 1 >= targetVer
         ? newObj as unknown as ExportObject[]
         : migrate(newObj, ver + 1, targetVer);
 }
